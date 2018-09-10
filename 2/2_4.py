06ac18d29f151f158f8f033b5655c843ef95e1a9
wangmin <wangminsuccess@gmail.com>
2018-09-10 23:30:57 +0800

    9_10_1

--- 2/2_4.py
+++ 2/2_4.py
@@ -1,6 +1,10 @@
 import re
 import urllib.request
 import urllib.parse
+import lxml.html
+import csv
+#learn from https://zhuanlan.zhihu.com/p/28621516
+#1、下载网站源码
 def download(url,user_agent='wswp',proxy=None,num_retries=2,timout=5):
     print('Begin Download:',url)
 
@@ -27,3 +31,46 @@ def download(url,user_agent='wswp',proxy=None,num_retries=2,timout=5):
         print('Exception as:',e)
         html=None
     return html
+#2、获取网页中的链接地址
+def get_links(html):
+    if html:
+        page_regex=re.compile('<a[^>+href=["\']',re.IGNORECASE)
+        return page_regex.findall(html.decode)
+    else:
+        return ""
+
+#编写爬取规则，解析获得数据
+def scrape_callback(url,html):
+    csslist =['span[property="v:itemreviewed"]','span.year','strong[property="v:average"]']
+    try:
+        tree=lxml.html.fromstring(html)
+        row=[tree.cssselect('{0}'.format(field))[0].text for field in csslist] # dont know
+        print(url,row)
+    except Exception as e:
+        print("Callback error as:",e)
+
+#爬虫入口
+def link_crawler(seed_url,link_regex,max_depth=2,scrape_callback=None):
+    crawler_queue=[seed_url] #爬取首页地址中特定链接，保存在队列中
+    seens={seed_url:1} #map 存取爬取深度
+
+    while crawler_queue:
+        url=crawler_queue.pop()
+        html=download(url)
+        depth=seens[url]
+        print("深度："depth)
+
+        for link in get_links(html):
+            if depth != max_depth and re.search(link_regex,link):
+                link=urllib.parse.urljoin(seed_url,link) #拼接 标准网页链接
+
+                if link not in seens:
+                    seens[link]=depth+1
+                    crawler_queue.append(link)
+
+        #有回调函数
+        if scrape_callback:
+            scrape_callback(url,html)
+
+class ScrapeCallback:
+    def __init__(self):
